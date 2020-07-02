089288973d7da176442cef58c3a99d9324ba4003
Dev.Nielsen <Frederik-N@hotmail.dk>
2020-07-02 13:47:14 +0200

    started new problem

---
+++ problem27.py
@@ -0,0 +1,11 @@
+# problem 27
+# Find the product of the coefficients, a and b,
+#  for the quadratic expression that produces 
+# the maximum number of primes for consecutive values of n, starting with  n=0
+
+def solution():
+    print("NOT SOLVED")
+    
+
+if __name__ == "__main__":
+    solution()
